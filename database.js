e5a34f03c694294277b9e20db4ae9403d047f9b1
Deare <179417343+Deare-stack@users.noreply.github.com>
2024-11-14 20:34:05 -0500

    mysql2,connected fortend with node.js

---
+++ database.js
@@ -0,0 +1,15 @@
+const { createPool } = require('mysql2');
+
+const pool = createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'java_cafe'
+});
+
+pool.query(`SELECT * FROM items`, (err, res) => {
+    if (err) {
+        return console.error('Error executing query:', err);
+    }
+    console.log('Query results:', res);
+});
