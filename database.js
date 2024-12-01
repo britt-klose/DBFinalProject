992d5f6d9138ddbe9cadbac2676c1543aad7fa4f
Deare <179417343+Deare-stack@users.noreply.github.com>
2024-11-30 20:54:59 -0500

    database connected

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const { createPool } = require('mysql2');
 const pool = createPool({
     host: 'localhost',
     user: 'root',
-    password: '1959832',
+    password: '123456',
     database: 'java_cafe'
 });
 
