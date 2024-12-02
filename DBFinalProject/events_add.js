7d1f029c4a70d1a43da6a4069ae436c72b27946c
Rtniewi <156935470+Rtniewi@users.noreply.github.com>
2024-12-02 17:19:24 -0500

    event changes

--- DBFinalProject/events_add.js
+++ DBFinalProject/events_add.js
@@ -12,52 +12,52 @@ document.addEventListener('DOMContentLoaded', function() {
       events: [
         {
           title: 'Open Mic',
-          href:'http://localhost:5501/DBFinalProject/event_registration.html',
+          url:'http://localhost:5501/DBFinalProject/event_registration.html',
           start: '2024-12-24T15:00:00'
         },
         {
           title: 'Painting Class',
-          url:'',
+          url:'http://localhost:5501/DBFinalProject/event_registration.html',
           start: '2025-01-06T14:30:00'
         },
         {
           title: 'Live Jazz',
-          url:'',
+          url:'http://localhost:5501/DBFinalProject/event_registration.html',
           start: '2025-01-13T14:00:00'
         },
         {
           title: 'Trivia',
-          url:'',
+          url:'http://localhost:5501/DBFinalProject/event_registration.html',
           start: '2025-01-20T15:00:00'
         },
         {
           title: 'Book Club',
-          url:'',
+          url:'http://localhost:5501/DBFinalProject/event_registration.html',
           start: '2025-01-28T11:30:00'
         },
         {
           title: 'Trivia',
-          url:'',
+          url:'http://localhost:5501/DBFinalProject/event_registration.html',
           start: '2025-02-03T15:00:00'
         },
         {
             title: 'Painting Class',
-            url:'',
+            url:'http://localhost:5501/DBFinalProject/event_registration.html',
             start: '2025-02-10T15:00:00'
         },
         {
             title: 'Karaoke',
-            url:'',
+            url:'http://localhost:5501/DBFinalProject/event_registration.html',
             start: '2025-02-18T14:30:00'
         },
         {
             title: 'Art Show',
-            url:'',
+            url:'http://localhost:5501/DBFinalProject/event_registration.html',
             start: '2025-02-24T14:30:00'
         },
         {
             title: 'Book Club',
-            url:'',
+            url:'http://localhost:5501/DBFinalProject/event_registration.html',
             start: '2025-02-25T11:30:00'
         }
       ]
