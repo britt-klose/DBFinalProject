224f82a54b75c0c74ad8a164dac5a54fa75be6c4
Deare <179417343+Deare-stack@users.noreply.github.com>
2024-12-02 01:54:49 -0500

    Co-authored-by: Brittany Klose <brittany.klose@yahoo.com> Co-authored-by: Rtniewi <Rtniewi@users.noreply.github.com>

--- database.js
+++ database.js
@@ -2,25 +2,25 @@
 const express = require('express'); // Express framework for handling HTTP requests
 const mysql = require('mysql2'); // MySQL2 client for Node.js
 const cors = require('cors'); // For web security
-const {authMiddleware} = require('.auth.js');
-// port=8081;
+//const {authMiddleware} = require('.auth.js');
+port=8081;
 // // Create an instance of express
-// const app = express();
-// app.use(cors()); // Enable CORS for the frontend React app
+const app = express();
+app.use(cors()); // Enable CORS for the frontend React app
 
 // // Create a connection to the MySQL database
-// const db = mysql.createConnection({
-//     host: "localhost", // Database host
-//     user: "root",      // Database username
-//     password: "123456", // Database password
-//     database: "java_cafe" // Name of the database
-// });
+const db = mysql.createConnection({
+host: "34.69.58.203", // Database host
+user: "root",      // Database username
+password: "123456", // Database password
+database: "cafe" // Name of the database
+});
 
 // // Define a route for the root URL '/'
-// app.get('/', (req, res) => {
+app.get('/', (req, res) => {
 //     // Respond with a JSON message
-//     return res.json("From backend side");
-// });
+return res.json("From backend side");
+});
 
 // // Define a route to fetch all items or search items by name
 // app.get('/items', (req, res) => {
@@ -33,23 +33,23 @@ const {authMiddleware} = require('.auth.js');
 // });
 
 
-// app.listen(port, () => {
-//     console.log(`Server is running on http://localhost:${port}`);
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+// const { createPool } = require('mysql2');
+
+// const pool = createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '123456',
+//     database: 'java_cafe'
 // });
 
-const { createPool } = require('mysql2');
+// pool.query(`SELECT * FROM items`, (err, res) => {
+//     if (err) {
+//         return console.error('Error executing query:', err);
+//     }
+//     console.log('Query testing:', res);
 
-const pool = createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '123456',
-    database: 'java_cafe'
-});
-
-pool.query(`SELECT * FROM items`, (err, res) => {
-    if (err) {
-        return console.error('Error executing query:', err);
-    }
-    console.log('Query testing:', res);
-
-});
+// });
